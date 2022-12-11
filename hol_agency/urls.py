47870dddcda1e2f701668f1b1c79ce82e479d58c
nazlidilara <b210109025@subu.edu.tr>
2022-12-11 20:57:36 +0300

    guncel

--- hol_agency/urls.py
+++ hol_agency/urls.py
@@ -20,7 +20,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('',include('home.urls')),
     path('home/', include('home.urls')),
+    path('product/',include('product.urls')),
     path('admin/', admin.site.urls),
 ]
 
