ae797a3f5087e38bb149e75a0558193cf8ecc8c8
nazlidilara <b210109025@subu.edu.tr>
2022-12-14 21:22:08 +0300

    guncel

--- hol_agency/urls.py
+++ hol_agency/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf.urls.static import static
+from django.conf import settings
+from home.views import index
 from django.contrib import admin
 from django.urls import include, path
 
@@ -24,5 +26,8 @@ urlpatterns = [
     path('home/', include('home.urls')),
     path('product/',include('product.urls')),
     path('admin/', admin.site.urls),
+    path(' ',index,name='index'),
 ]
 
+urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns +=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
